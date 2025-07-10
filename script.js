971d25094d8c0f2b66a1e887596a4afbed7bf8f4
SemprulCode <bryanggbet71@gmail.com>
2025-07-10 21:59:00 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+const paymentContainer = document.getElementById('payment-container');
+const metodePembayaran = document.getElementById('metode-pembayaran');
+const bayarButton = document.getElementById('bayar');
+
+bayarButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const metode = metodePembayaran.value;
+    if (metode === 'dana') {
+        // Kode untuk pembayaran Dana
+        paymentContainer.innerHTML = `
+            <h2>Pembayaran melalui Dana</h2>
+            <p>Silakan melakukan pembayaran ke nomor ${                }</p>
+        `/* 089649986355 */}</p>
+        `;
+    } else if (metode === 'gopay') {
+        // Kode untuk pembayaran Gopay
+        paymentContainer.innerHTML = `
+            <h2>Pembayaran melalui Gopay</h2>
+            <p>Silakan melakukan pembayaran ke nomor ${                 }</p>
+        `/* 089649986355 */}</p>
+        `;
+    } else if (metode === 'qris') {
+        // Kode untuk pembayaran QRIS
+        paymentContainer.innerHTML = `
+            <h2>Pembayaran melalui QRIS</h2>
+            <img src="${                 }" alt="QR Code">
+        `/* https://files.catbox.moe/daqljh.jpg */}" alt="QR Code">
+        `;
+    }
+});
